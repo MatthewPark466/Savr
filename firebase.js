4da46db88b5ffb11abee628b06ed3167ff4b7487
benjaminoh10 <benjamin32193@gmail.com>
2025-09-18 17:31:34 +0000

    customer login/sign up

---
+++ firebase.js
@@ -0,0 +1,62 @@
+import { initializeAppp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { 
+
+    getAuth,
+    createUserWithEmailPassword,
+    signInWithEmainPassword,
+    signOut,
+    onAuthStateChanged
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import {
+    getFirestore,
+    doc,
+    setDoc
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export async funtion signup(email, password, role) {
+    const cred = await createUserWithEmailPassword(auth, email, password);
+    await setDoc(doc(db, "users", cred.user.uid), {
+        email,
+        role,
+        createdAt: Date.now()
+
+    });
+    return cred.user;
+}
+
+//login
+export async function login(email, password) {
+    const cred = await signInWithEmainPassword(auth, email, password);
+    return cred.user;
+}
+
+//logout
+export async function logout() {
+    await signOut(auth);
+}
+
+export function onAuthChange(callback) {
+    return onAuthStateChanged(auth, callback);
+}
+
+export { auth, db };
